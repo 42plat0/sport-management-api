d42b5edf842135ce4071c02d3c24505897ff9f0d
penguin-wings <gytisnaujasis@icloud.com>
2025-03-07 16:10:43 +0200

    clean up code and export functions in exports

--- src/models/sportModel.js
+++ src/models/sportModel.js
@@ -1,18 +1,8 @@
-const fs = require("fs");
-const {getData} = require("../helpers/getFileData");
-const {getSportJson, getPlayerJson } = require("../helpers/getSportJson");
-const {updateObj} = require("../helpers/updateObj");
 const {sql} = require("../db.js");
 
-const validKeys = {
-	"player": ["name", "age", "position"],
-	"sport": ["name", "popularityRank"]
-}
-
-let sports = getData();
-
-const getSports = async () =>{
-    const sportS = await sql`
+// Gett all sports
+exports.getSports = async () =>{
+    const sports = await sql`
         SELECT s.id, 
                s.name, 
                s.popularityRank,
@@ -22,12 +12,12 @@ const getSports = async () =>{
                ON s.id = p.sport_id
         GROUP BY s.id
         ORDER BY s.id ASC;
-    `
-    
-    return sportS;
+    ` 
+    return sports;
 }
 
-const insertSport = async (sport) => {
+// Insert new sport with distinct name
+exports.insertSport = async (sport) => {
     const [newSport] = await sql`
         INSERT INTO sports (name, popularityRank)
         VALUES (${sport.name}, ${sport.popularityRank})
@@ -36,7 +26,8 @@ const insertSport = async (sport) => {
     return newSport;
 }
 
-const fetchSport = async (sId) => {
+// Get sport by id
+exports.fetchSport = async (sId) => {
     const [sport] = await sql`
         SELECT s.id, 
                s.name, 
@@ -51,7 +42,8 @@ const fetchSport = async (sId) => {
     return sport;
 }
 
-const updateSportDb = async (sId, updatedSport) => {
+// Update sport chosen by id
+exports.updateSportDb = async (sId, updatedSport) => {
 
     const [sport] = await sql`
         UPDATE sports
@@ -64,7 +56,7 @@ const updateSportDb = async (sId, updatedSport) => {
     return sport;
 }
 
-const deleteSportDb = async (sId) => {
+exports.deleteSportDb = async (sId) => {
     const [sport] = await sql`
         DELETE FROM sports
         WHERE id = ${sId}
@@ -72,144 +64,4 @@ const deleteSportDb = async (sId) => {
     `
     return sport;
 }
-
-const isCorrectInp = (inp, validKeys) =>{
-	// Input:
-	// 		inp object
-	// 		valid key list
-	// Output:
-	// 		0 - keys match
-	// 		1 - keys dont match
-	
-	const inpKeys = Object.keys(inp);	
-	
-	for (const key in validKeys){
-		// Not enough or invalid keys
-		if (!validKeys.includes(inpKeys[key]))
-			return false;
-	}
-	return true;
-}
-
-const add = (inpObj, sIdx=undefined) => {
-	// Input:
-	// 		sport or player object
-	// 		OPTIONALLY: sport_id
-	// Output:
-	// 		0 if successfully added
-	// 		1 if couldn't add
-	try{
-		if (sIdx >= 0 ){
-			// Add player
-			// Get sport and last player to pick id for new one from
-			const sport = sports[sIdx]
-			const lastPlayer = sport.players[sport.players.length-1];
-			
-			// Check if req body is correct
-			if (!isCorrectInp(inpObj, validKeys.player))
-				throw new Error("Invalid keys");
-
-			inpObj = getPlayerJson(inpObj,lastPlayer);
-			sports[sIdx].players.push(inpObj);
-		}
-		// Add sport
-		else{
-			if (!isCorrectInp(inpObj, validKeys.sport))
-				throw new Error("Invalid keys");
-			
-			inpObj = getSportJson(inpObj, sports[sports.length - 1]);
-				
-			sports.push(inpObj);
-		}
-		writeFile(sports);
-		sports = getData();		// Update sports
-		return 0;
-	} catch(error){
-		console.error(error.message);
-		return 1;
-	}
-}
-
-const upd = (oldObjIdx, updObj, sId=undefined) => {
-	// Input:
-	// 		old obj index
-	// 		updated obj
-	// 		OPTIONAL: sport_id 
-	// Output:
-	// 		0 if successfully updated
-	// 		1 if couldn't update
-
-	try{
-		// Player
-		if (sId >= 0){
-			let oldObj = sports[sId].players[oldObjIdx];
-			oldObj = updateObj(oldObj, updObj);
-			sports[sId].players[oldObjIdx] = oldObj;
-		}
-		// Sport
-		else{
-			let oldObj = sports[oldObjIdx];
-			oldObj = updateObj(oldObj, updObj);
-			sports[oldObjIdx] = oldObj;
-		}
-
-		writeFile(sports);
-		sports = getData();		// Update sports
-		return 0;
-	} catch(error){
-		console.error(error.message);
-		return 1;
-	}
-}
-
-const del = (sIdx, pId=undefined) => {
-	// Input:
-	// 		sport index
-	// 		OPTIONAL: player_id 
-	// Output:
-	// 		0 if successfully deleted
-	// 		1 if couldn't delete
-
-	try{
-		let updObj;
-		// Player
-		if (pId >= 0){
-			updObj = [...sports];
-			const updPlayers = updObj[sIdx].players.filter((player) => player.id !== pId);
-
-			updObj[sIdx].players = updPlayers;
-		}
-		// Sport
-		else{
-			updObj = sports.filter((sport) => sport.id !== sports[sIdx].id);	
-		}
-		writeFile(updObj);
-		sports = getData();		// Update sports
-		return 0;
-
-	} catch(error){
-		console.error(error.message);
-		return 1;
-	}
-}
-
-const writeFile = (data) => {
-	// Input:
-	// 		data to write
-	// Output:
-	// 		0 if successfully written
-	// 		1 if couldn't write
-
-	try{
-		const content = JSON.stringify(data);
-		fs.writeFileSync(__dirname + "/../data/sports.json", content);
-		return 0;
-	} catch(error){
-		console.error(error.message);
-		return 1;
-	}
-}
-
-
-module.exports = {sports, add, upd, del, getSports, insertSport, fetchSport, updateSportDb, deleteSportDb};
 
